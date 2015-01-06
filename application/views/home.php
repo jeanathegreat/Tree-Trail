26938eb5fcdae48cd47de7602b5255bdba077d0a
Joseph <fskreuz@gmail.com>
2015-01-06 20:02:09 +0000

    Adding jQuery and bootstrap for initial styling and tooling

--- application/views/home.php
+++ application/views/home.php
@@ -9,12 +9,24 @@
   <link rel="import" href="<?= base_url("static/components/tree-map.html"); ?>">
   <link rel="import" href="<?= base_url("static/components/side-bar.html"); ?>">
 
+  <link rel="stylesheet" href="<?= base_url('static/libs/bootstrap/dist/css/bootstrap.min.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('static/libs/bootstrap/dist/css/bootstrap-theme.min.css'); ?>">
+
+  <style>
+    html, body {
+      position:relative;
+      width : 100%;
+      height: 100%;
+    }
+  </style>
+
 </head>
 <body>
 
   <tree-map latitude="100" longitude="200"></tree-map>
-  <side-bar></side-bar>
+  <!-- <side-bar></side-bar> -->
 
-
+  <script src="<?= base_url('static/libs/jquery/dist/jquery.min.js'); ?>"></script>
+  <script src="<?= base_url('static/libs/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
 </body>
 </html>
